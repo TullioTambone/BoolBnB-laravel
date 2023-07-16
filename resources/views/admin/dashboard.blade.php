61652c8e125aecc1f39a6f50f6e365d05663fcb0
TullioTambone <tulliotambone97@gmail.com>
2023-07-16 11:20:53 +0200

    modifiche dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,12 +3,16 @@
 @section('content')
 <div class="container">
     <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
+        {{ __('Benvenuto '. $user->name.' questa è la tua Dashboard') }}
     </h2>
     <div class="row justify-content-center">
-        <div class="col">
+        <div class="col-12">
             <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+                <div class="card-header">
+                    <h1>
+                        {{ __($user->name .' '. $user->surname) }}
+                    </h1>
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -18,6 +22,9 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    <p>
+                        Benvenuto nella tua Dashboard personale! Questo è il centro di controllo dove puoi gestire tutti gli aspetti dei tuoi servizi su Airbnb. Qui potrai tenere traccia di tutte le informazioni importanti relative ai tuoi appartamenti e offerte.
+                    </p>
                 </div>
             </div>
         </div>
