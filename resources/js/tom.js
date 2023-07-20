0f255e68aabd9e6de488788d91c619e5e9882332
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-07-20 12:58:26 +0200

    Validazione lato client side

--- resources/js/tom.js
+++ resources/js/tom.js
@@ -1,4 +1,27 @@
+// //    PROVE PER INTEGRAZIONE MAPPA E PUNTATORE MAPPA
+    
+//     document.getElementById('mappa').innerHTML = `<div id="map" class="map position-relative" style="width=500px; height=500px;"></div>`;
+//     //document.querySelector('.mapboxgl-marker').classList.add("position-absolute");
+//     point =  [elem.position.lng, elem.position.lat];
+//     if (!map) {
+//         // Inizializza la mappa solo se non è stata già creata
+//         map = tt.map({
+//             key: "74CVsbN34KoIljJqOriAYN2ZMEYU1cwO",
+//             center: point, // Inverti la latitudine e longitudine per la posizione corretta
+//             container: "map",
+//             zoom: 15,
+//         });
+//     }
+
+//     // Aggiorna la posizione del marker se esiste
+//     if (marker) {
+//         marker.setLngLat([elem.position.lng, elem.position.lat]);
+//     } else {
+//         // Crea il marker solo se non esiste
+//         marker = new tt.Marker().setLngLat([elem.position.lng, elem.position.lat]).addTo(map);
+//     }
 // importazione tom tom
+
 import tt from "@tomtom-international/web-sdk-maps";
 
 import ttServices from "@tomtom-international/web-sdk-services"
@@ -10,15 +33,25 @@ console.log('ciaone');
 // let marker = null;
 // let point = null;
 // Ottenimento del riferimento al form
+
+// form create
 const form = document.querySelector('#form-create');
-console.log(form);
+
+// chekboxes
+const checkboxes = form.querySelectorAll('input[type="checkbox"]');
+
+// input address
 const formAddress = document.querySelector('#address');
+
+let isAddressOk = false;
+let isAnyCheckboxChecked = false;
+
 // Aggiunta di un gestore di eventi al submit del form
 formAddress.addEventListener('keyup', async (e) => {
     e.preventDefault(); // Previeni il comportamento di default del submit
 
     // Ottenimento dell'indirizzo dal campo input
-    const address = document.querySelector('#address').value;
+    const address = document.querySelector('#address').value;    
 
     // se address esiste
     if( address ) {
@@ -41,37 +74,17 @@ formAddress.addEventListener('keyup', async (e) => {
 
                     if (elem.address.freeformAddress !== formAddress.value) {
                         formAddress.setAttribute('class', 'form-control is-invalid');
+
+                        isAddressOk = false;
                     } else {
                         formAddress.setAttribute('class', 'form-control is-valid');
-                        
-                    // //    PROVE PER INTEGRAZIONE MAPPA E PUNTATORE MAPPA
-                        
-                    //     document.getElementById('mappa').innerHTML = `<div id="map" class="map position-relative" style="width=500px; height=500px;"></div>`;
-                    //     //document.querySelector('.mapboxgl-marker').classList.add("position-absolute");
-                    //     point =  [elem.position.lng, elem.position.lat];
-                    //     if (!map) {
-                    //         // Inizializza la mappa solo se non è stata già creata
-                    //         map = tt.map({
-                    //             key: "74CVsbN34KoIljJqOriAYN2ZMEYU1cwO",
-                    //             center: point, // Inverti la latitudine e longitudine per la posizione corretta
-                    //             container: "map",
-                    //             zoom: 15,
-                    //         });
-                    //     }
-        
-                    //     // Aggiorna la posizione del marker se esiste
-                    //     if (marker) {
-                    //         marker.setLngLat([elem.position.lng, elem.position.lat]);
-                    //     } else {
-                    //         // Crea il marker solo se non esiste
-                    //         marker = new tt.Marker().setLngLat([elem.position.lng, elem.position.lat]).addTo(map);
-                    //     }
-        
+
+                        isAddressOk = true;
                     }
                 })    
     
                 // Verifica se ci sono risultati validi
-                if (results && results.length > 0) {
+                if (isAddressOk) {
                     // Ottenimento delle coordinate di latitudine e longitudine
                     const latitude = results[0].position.lat;
                     const longitude = results[0].position.lng;
@@ -82,23 +95,54 @@ formAddress.addEventListener('keyup', async (e) => {
             
                     console.log(latitude,longitude)
 
-                                            
-
-                
                     // Invia il form
-                    form.addEventListener('submit', (e) =>{
-                        // e.preventDefault();
-                        form.submit();
-                    })
+                    // form.addEventListener('submit', () =>{
+                    //     form.submit();
+                    // })
                     // form.submit();
                 } else {
                     console.error('Nessun risultato trovato per l\'indirizzo fornito.');
                 }
             }
         );
-    } else {
-        // Invia il form comunque ma senza indirizzo
-        form.submit();
     }
+});
 
+
+const checkFeed = document.getElementById('checkbox-feedback')
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); // Previeni l'invio del form inizialmente
+
+    checkboxes.forEach(checkbox => {
+
+        // se un checkbox is checked diventa true 
+        if (checkbox.checked) {
+            isAnyCheckboxChecked = true;
+            checkbox.setAttribute('class', 'form-check-input is-valid');
+        } else {
+            checkbox.setAttribute('class', 'form-check-input is-invalid');
+        }
+
+        if (!isAnyCheckboxChecked) {
+    
+            // Mostra un messaggio di errore, ad esempio sotto il gruppo di checkbox
+            checkFeed.style.display = 'block';
+            checkFeed.innerHTML = `Seleziona almeno un campo dei Servizi`;
+        } else if(isAnyCheckboxChecked) {
+            isAnyCheckboxChecked = true;
+            checkFeed.style.display = 'none'
+        }
+
+        // checkbox.addEventListener('change', function () {
+        //     if (isAnyCheckboxChecked) {
+        //         checkbox.setAttribute('class', 'form-check-input');
+        //     }
+        // });
+    });
+
+    if (isAnyCheckboxChecked && isAnyFeedbackChecked) {
+        form.submit();
+            // Invia il form solo se almeno un checkbox è selezionato
+    }
 });
--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -27,8 +27,7 @@
                     {{-- title --}}
                     <div>
                         <label for="title">Titolo *</label>
-                        <input class="form-control" @error('title') is-invalid  @enderror type="text" id="title" name="title" 
-                        value="{{ old('title')}}" autocomplete="off">
+                        <input class="form-control" @error('title') is-invalid  @enderror type="text" id="title" name="title" value="{{ old('title') }}" autocomplete="off">
                         @error('title')
                             <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
@@ -43,7 +42,7 @@
                     {{-- rooms --}}
                     <div>
                         <label for="rooms">Stanze *</label>
-                        <input class="form-control" @error('rooms') is-invalid  @enderror type="number" id="rooms" name="rooms" min="0" required value="{{ old('rooms')}}">
+                        <input class="form-control" @error('rooms') is-invalid  @enderror type="number" id="rooms" name="rooms" min="0" required>
                         <div class="invalid-feedback">Non puoi inserire un numero negativo!</div>
                         @error('rooms')
                             <div class="alert alert-danger">{{ $message }}</div>
@@ -52,8 +51,8 @@
 
                     {{-- bedrooms --}}
                     <div>
-                        <label for="bedrooms">Camere da letto *</label>
-                        <input class="form-control" @error('bedrooms') is-invalid  @enderror type="number" id="bedrooms" name="bedrooms" min="0" required value="{{ old('bedrooms')}}">
+                        <label for="bedrooms">Camera da letto *</label>
+                        <input class="form-control" @error('bedrooms') is-invalid  @enderror type="number" id="bedrooms" name="bedrooms" min="0">
                         <div class="invalid-feedback">Non puoi inserire un numero negativo!</div>
                         @error('bedrooms')
                             <div class="alert alert-danger">{{ $message }}</div>
@@ -63,7 +62,7 @@
                     {{-- bathrooms --}}
                     <div>
                         <label for="bathrooms">Bagni *</label>
-                        <input class="form-control" @error('bathrooms') is-invalid  @enderror type="number" id="bathrooms" name="bathrooms" min="0" required value="{{ old('bathrooms')}}">
+                        <input class="form-control" @error('bathrooms') is-invalid  @enderror type="number" id="bathrooms" name="bathrooms" min="0">
                         <div class="invalid-feedback">Non puoi inserire un numero negativo!</div>
                         @error('bathrooms')
                             <div class="alert alert-danger">{{ $message }}</div>
@@ -73,7 +72,7 @@
                     {{-- square meters --}}
                     <div>
                         <label for="square_meters">Metri quadrati *</label>
-                        <input class="form-control" @error('square_meters') is-invalid  @enderror type="number" id="square_meters" name="square_meters" min="0" required value="{{ old('square_meters')}}">
+                        <input class="form-control" @error('square_meters') is-invalid  @enderror type="number" id="square_meters" name="square_meters" min="0">
                         <div class="invalid-feedback">Non puoi inserire un numero negativo!</div>
                         @error('square_meters')
                             <div class="alert alert-danger">{{ $message }}</div>
@@ -85,7 +84,7 @@
                          --}}
                     <div>
                         <label for="address">Indirizzo *</label>
-                        <input list="data" class="form-control" @error('address') is-invalid  @enderror type="text" id="address" name="address" autocomplete="off" required value="{{ old('address')}}">
+                        <input list="data" class="form-control" @error('address') is-invalid  @enderror type="text" id="address" name="address" autocomplete="off">
                         @error('address')
                             <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
@@ -124,8 +123,9 @@
                     {{-- servizi --}}
                     <div class="mb-3">
                         <label for="services">Servizi *</label>
+                        <div id="checkbox-feedback" class="invalid-feedback"></div>
                         @foreach ($services as $element)
-                        <div id="checkbox-container">
+                        <div>
                             <label for="check-service-{{ $element->id }}" class="form-label">
                                 {{ $element->name }}
                                 <i class="fa-solid {{ $element->icon }}"></i>
