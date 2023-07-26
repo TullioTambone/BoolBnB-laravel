e76d65fbf48ab31f999fca35b00d8109542157d2
TullioTambone <tulliotambone97@gmail.com>
2023-07-26 12:02:42 +0200

    validazione radios

--- resources/js/braintree.js
+++ resources/js/braintree.js
@@ -9,18 +9,38 @@ braintree.dropin.create({
     
     form.addEventListener('submit', event => {
         event.preventDefault();
+        
+        event.preventDefault();
+        let radioButtons = document.querySelectorAll("input[type='radio']");
+        let isChecked = false;
+        
+        for (const elem of radioButtons) {
+            
+            // Interrompi il ciclo se trovi una corrispondenza
+            if (elem.checked) {
+                isChecked = true;
+                elem.setAttribute('class', 'btn-check is-valid');
+                break; 
+    
+            } else {
+                isChecked = false;
+                elem.setAttribute('class', 'btn-check is-invalid');
+            }
+        }
 
-        dropinInstance.requestPaymentMethod((error, payload) => {
-        if (error) console.error(error);
-
-
-        // Step four: when the user is ready to complete their
-        //   transaction, use the dropinInstance to get a payment
-        //   method nonce for the user's selected payment method, then add
-        //   it a the hidden field before submitting the complete form to
-        //   a server-side integration
-        document.getElementById('nonce').value = payload.nonce;
-        form.submit();
-        });
+        if (isChecked) {     
+            dropinInstance.requestPaymentMethod((error, payload) => {
+                if (error) console.error(error);
+    
+    
+                // Step four: when the user is ready to complete their
+                //   transaction, use the dropinInstance to get a payment
+                //   method nonce for the user's selected payment method, then add
+                //   it a the hidden field before submitting the complete form to
+                //   a server-side integration
+                document.getElementById('nonce').value = payload.nonce;
+                form.submit();
+            });
+        }
     });
 });
