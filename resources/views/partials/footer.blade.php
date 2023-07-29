8cafca04c32bd35d738854e2ff44cf75cc45b642
TullioTambone <tulliotambone97@gmail.com>
2023-07-29 18:06:55 +0200

    adj colors - footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,67 +1,69 @@
+<footer>
 
-<div class="contenitore">
-    <div class="contenitore2 d-flex justify-content-between ">
-        <div class="p-3">
-            <img class="logo-img" src="{{ asset('img/Boolbnb-logo-transparente-total-white.png') }}"" alt="Boolbnb logo">
-        </div> 
-        <div class="d-flex p-3">
-                <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                <li><a href="#"><i class="fa-brands fa-linkedin"></i></a></li>
-                <li><a href=""><i class="fa-brands fa-youtube"></i></a></li>
+    <div class="contenitore">
+        <div class="contenitore2 d-flex justify-content-between ">
+            <div class="p-3">
+                <img class="logo-img" src="{{ asset('img/Boolbnb-logo-transparente-total-white.png') }}"" alt="Boolbnb logo">
+            </div> 
+            <div class="d-flex p-3">
+                    <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-linkedin"></i></a></li>
+                    <li><a href=""><i class="fa-brands fa-youtube"></i></a></li>
+            </div>
         </div>
-    </div>
-    <div class="contenitore3 d-flex">
-        <div>
-            <ul class="mt-3 p-3">
-                <li>
-                    SOCIETA'
-                </li>
-                <li>
-                    <a href="">Posizioni aperte</a>
-                </li>
-                <li>
-                   <a href="">Stampa</a> 
-                </li>
-                <li> <a href="">Relazioni con gli investitori</a> </li>
-                <li> <a href="">Boolbnb App - Ovunque tu sia</a> </li>
-                <li> <a href="">Boolbnb Business Studio</a> </li>
-            </ul>
+        <div class="contenitore3 d-flex">
+            <div>
+                <ul class="mt-3 p-3">
+                    <li>
+                        SOCIETA'
+                    </li>
+                    <li>
+                        <a href="">Posizioni aperte</a>
+                    </li>
+                    <li>
+                       <a href="">Stampa</a> 
+                    </li>
+                    <li> <a href="">Relazioni con gli investitori</a> </li>
+                    <li> <a href="">Boolbnb App - Ovunque tu sia</a> </li>
+                    <li> <a href="">Boolbnb Business Studio</a> </li>
+                </ul>
+            </div>
+            <div>
+                <ul class="mt-3 p-3">
+                    <li>AIUTO</li>
+                    <li> <a href="">Scopri come funziona Boolbnb</a> </li>
+                    <li> <a href="">Termini e condizioni</a> </li>
+                    <li> <a href="">Note legali</a> </li>
+                    <li> <a href="">Preferenze sui cookie</a> </li>
+                    <li> <a href="">Informativa sulla privacy</a> </li>
+                    <li> <a href="">Cyber Security</a> </li>
+                    <li> <a href="">COVID-19: consigli per i viaggiatori</a> </li>
+                </ul>
+            </div> 
+            <div>
+                <ul class="mt-3 p-3">
+                    <li>
+                        INFORMAZIONI SU BOOLBNB
+                    </li>
+                    <li>
+                    <a href="">News</a> 
+                    </li>
+                    <li>
+                    <a href="">Lavora con noi</a>  
+                    </li>
+                    <li>
+                    <a href="">Investitori</a> 
+                    </li>
+                    <li>
+                    <a href="">Sostenibilità</a> 
+                    </li>
+                </ul>
+            </div>
         </div>
-        <div>
-            <ul class="mt-3 p-3">
-                <li>AIUTO</li>
-                <li> <a href="">Scopri come funziona Boolbnb</a> </li>
-                <li> <a href="">Termini e condizioni</a> </li>
-                <li> <a href="">Note legali</a> </li>
-                <li> <a href="">Preferenze sui cookie</a> </li>
-                <li> <a href="">Informativa sulla privacy</a> </li>
-                <li> <a href="">Cyber Security</a> </li>
-                <li> <a href="">COVID-19: consigli per i viaggiatori</a> </li>
-            </ul>
-        </div> 
-        <div>
-            <ul class="mt-3 p-3">
-                <li>
-                    INFORMAZIONI SU BOOLBNB
-                </li>
-                <li>
-                <a href="">News</a> 
-                </li>
-                <li>
-                <a href="">Lavora con noi</a>  
-                </li>
-                <li>
-                <a href="">Investitori</a> 
-                </li>
-                <li>
-                <a href="">Sostenibilità</a> 
-                </li>
-            </ul>
+        <div class="contenitore2 mt-3 d-flex justify-content-center ">
+            <span> <i class="fa-solid fa-location-dot"></i> italia  © 2023 Boolbnb, Inc. Tutti i diritti riservati </span>
         </div>
-    </div>
-    <div class="contenitore2 mt-3 d-flex justify-content-center ">
-        <span> <i class="fa-solid fa-location-dot"></i> italia  © 2023 Boolbnb, Inc. Tutti i diritti riservati </span>
-    </div>
- </div>
+     </div>
+</footer>
